9b633539152effdc3b876dfb04f1a425b85f92cf
Serhii Basok <sergeybsk19@gmail.com>
2025-07-05 18:20:34 +0300

    crete copy file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    if parts[1] == parts[2]:
+        return
+    try:
+        with open(parts[1], "r") as file_out, open(parts[2], "w") as file_in:
+            file_in.write(file_out.read())
+    except FileNotFoundError:
+        return
